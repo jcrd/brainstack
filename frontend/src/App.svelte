d5cf599883641027f522b08c1870da7044ba0804
James Reed <james@twiddlingbits.net>
2024-05-21 08:35:00 -0600

    Focus new stack on creation

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -77,6 +77,7 @@
                                 order,
                             },
                         ]
+                        $tabSet = $stacks.length - 1
                     })
                     .catch(catchError)
             },
