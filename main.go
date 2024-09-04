dda4730302fc1c2be053d8401ed5c605184624a5
James Reed <james@twiddlingbits.net>
2024-09-04 13:37:51 -0600

    Change default window size

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "brainstack",
-		Width:  525,
+		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
