49f3833e454616483d26c699f7630a602b1df575
James Reed <james@twiddlingbits.net>
2024-09-06 13:24:29 -0600

    Fix non-existent tag selections

--- frontend/src/components/Tag.svelte
+++ frontend/src/components/Tag.svelte
@@ -4,9 +4,13 @@
     export let tag
 </script>
 
-<button
-    on:click={() => $tagSelections[tag.stack_id][tag.ID] = !$tagSelections[tag.stack_id][tag.ID]}
-    class="chip {$tagSelections[tag.stack_id][tag.ID] ? 'variant-filled' : 'variant-soft'}"
->
-    #{tag.name}
-</button>
+<div>
+    {#if $tagSelections[tag.stack_id]}
+        <button
+            on:click={() => $tagSelections[tag.stack_id][tag.ID] = !$tagSelections[tag.stack_id][tag.ID]}
+            class="chip {$tagSelections[tag.stack_id][tag.ID] ? 'variant-filled' : 'variant-soft'}"
+        >
+            #{tag.name}
+        </button>
+    {/if}
+</div>
